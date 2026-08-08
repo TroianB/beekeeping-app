24678ed0762cc3e743de23b1256726857a432128
TroianB <136427600+TroianB@users.noreply.github.com>
2026-08-09 02:40:58 +1200

    Load screen fit styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import BeekeepingApp from "./App.jsx";
+import "./fit-screen.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
